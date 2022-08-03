cd0325b678f426bdfdb19a6a8af4f3cc43905493
Harpreet-Narwal <62388865+Harpreet-Narwal@users.noreply.github.com>
2022-08-03 10:05:36 +0530

    Update webscreapper.py

--- webscreapper.py
+++ webscreapper.py
@@ -1,9 +1,12 @@
 from ast import keyword
+from email import header
 from random import betavariate
 from re import X
 from turtle import title
 import requests
 from bs4 import BeautifulSoup
+from csv import writer
+
 
 headers = {'User-agent': 'Chrome/103.0.5060.134'}
 
@@ -13,33 +16,40 @@ html = request.content
 soup = BeautifulSoup(html, 'html.parser')
 
 # print(soup.prettify)
-
 def toi_news_scrapper(keyword):
     news_list = []
+    with open('news_scraping.csv', 'w', encoding='utf8', newline='') as f:
+        thewriter = writer(f)
+        header = ['Index', 'Head Lines', 'Keyword']
+        thewriter.writerow(header)
+        for h in soup.find_all('a',  class_="linktype1" ):
+            for i in soup.find('figcaption'):
+                news_title = h.contents[0].text
 
-    for h in soup.find_all('a',  class_="linktype1" ):
-        for i in soup.find('figcaption'):
-            news_title = h.contents[0].text
+            if news_title not in news_list:
+                    if 'Times of India' not in news_title:
+                        news_list.append(news_title)
 
-        if news_title not in news_list:
-                if 'Times of India' not in news_title:
-                    news_list.append(news_title)
+        no_of_news = 0
+        keyword_list = []
+        # Goes through the list and searches for the keyword
+        for i, title in enumerate(news_list):
+            text = ''
+            if keyword in title:
+                text = ' <------------ KEYWORD'
+                no_of_news += 1
+                keyword_list.append(title)
 
-    no_of_news = 0
-    keyword_list = []
-    # Goes through the list and searches for the keyword
-    for i, title in enumerate(news_list):
-        text = ''
-        if keyword in title:
-            text = ' <------------ KEYWORD'
-            no_of_news += 1
-            keyword_list.append(title)
+            info = [i + 1, title, text]
+            thewriter.writerow(info)
 
-        print(i + 1, ':', title, text)
+        # Prints the Titles of the articles that contain the keywords
+        info1 = [keyword, no_of_news]
+        thewriter.writerow(info1)
+        for i, title in enumerate(keyword_list):
+            info = [i + 1, title]
+            thewriter.writerow(info)
 
-    # Prints the Titles of the articles that contain the keywords
-    print(f'\n--------- Total mentions of "{keyword}" = {no_of_news} ---------')
-    for i, title in enumerate(keyword_list):
-        print(i + 1, ':', title)
-x = input("Enter what you want to search about: ")
-toi_news_scrapper(x)
+toi_news_scrapper('Covid')
+
+
