0c7b08b7dcd64dc370a9919c4fbaccde670669e4
Harpreet-Narwal <62388865+Harpreet-Narwal@users.noreply.github.com>
2022-08-12 20:32:06 +0530

    Update webscreapper.py

--- webscreapper.py
+++ webscreapper.py
@@ -6,6 +6,7 @@ from turtle import title
 import requests
 from bs4 import BeautifulSoup
 from csv import writer
+import re
 
 
 headers = {'User-agent': 'Chrome/103.0.5060.134'}
@@ -14,6 +15,10 @@ request = requests.get('https://timesofindia.indiatimes.com/', headers=headers)
 html = request.content
 
 soup = BeautifulSoup(html, 'html.parser')
+
+for i in range(1):
+    x = input("Enter The news you want to search for: ")
+
 
 # print(soup.prettify)
 def toi_news_scrapper(keyword):
@@ -36,7 +41,7 @@ def toi_news_scrapper(keyword):
         for i, title in enumerate(news_list):
             text = ''
             if keyword in title:
-                text = ' <------------ KEYWORD'
+                text = ' <---------- KEYWORD'
                 no_of_news += 1
                 keyword_list.append(title)
 
@@ -44,12 +49,24 @@ def toi_news_scrapper(keyword):
             thewriter.writerow(info)
 
         # Prints the Titles of the articles that contain the keywords
+    with open('Searched_news.csv', 'w',encoding='utf8', newline='') as f:
+        thewriter = writer(f)
+        header1 = ['Keyword', 'Number of articles found']
+        thewriter.writerow(header1)
         info1 = [keyword, no_of_news]
         thewriter.writerow(info1)
+        header2 = ['Searched News', 'Article Link']
+        thewriter.writerow(header2)
+            # links_list = soup.find_all('a')
         for i, title in enumerate(keyword_list):
-            info = [i + 1, title]
-            thewriter.writerow(info)
-for i in range(1):
-    x = input("Enter The news you want to search for: ")
-toi_news_scrapper(x)
+            # # for link in soup.findAll('a', attrs={'href': re.compile("^https://timesofindia.indiatimes.com/")}):
+                # for link in links_list:
+                #     if 'href' in link.attrs:
+            for link in soup.find_all('a', attrs={'href': re.compile("^https://")}):
+        # display the actual urls
+        # print(link.get('href')) 
+                info = [title, link.get('href')]
+                thewriter.writerow(info)                    
+                
 
+toi_news_scrapper(x)
