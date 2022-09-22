1611027712b2f5dfd851fdb1a257a39eb6daf368
Rudrakshi Gupta <109859631+somethings-fishy@users.noreply.github.com>
2022-09-22 23:53:53 +0530

    Deleted dir

--- web_scrapper/abc.py
+++
@@ -1,7 +0,0 @@
-import csv
-import imp
-import pandas as pd
-filename = 'web_scrapper\data.csv'
-
-r = pd.read_csv(filename,nrows=-3)
-print(r)
--- web_scrapper/call.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-from testing import analyse
-
-analyse.recentanalysis('Python_project\web_scrapper\data.csv')
-analyse.toptenanalysis('Python_project\web_scrapper\data.csv')
